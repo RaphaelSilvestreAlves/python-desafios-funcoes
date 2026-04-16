624345b376ac4f08e32a3052b45aabe8b0e35a41
Raphael Silvestre Alves <ssevlaa@gmail.com>
2026-04-16 13:26:47 -0300

    add: conta_caracteres

---
+++ contador_caracteres.py
@@ -0,0 +1,13 @@
+'''
+Sara está participando de um concurso de escrita,
+ e uma das regras exige que cada palavra de seu texto 
+ tenha um limite máximo de caracteres.
+
+Ajude Sara criando uma função que receba uma palavra e exiba a quantidade de caracteres.
+'''
+
+def conta_caracteres(palavra):
+    return len(palavra)
+
+palavra = input('Digite uma palavra: ')
+print(f'Essa palavra tem {conta_caracteres(palavra)} caracteres')
