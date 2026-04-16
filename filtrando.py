a132dd20ced344ee5c29c871daa2fc246fc93d65
Raphael Silvestre Alves <ssevlaa@gmail.com>
2026-04-16 14:15:02 -0300

    add: filtrando

---
+++ filtrando.py
@@ -0,0 +1,13 @@
+'''
+Lucas está desenvolvendo um sistema para gerar relatórios financeiros 
+e precisa filtrar apenas os valores pares de uma lista de números informada pelo usuário.
+
+Crie um programa que receba uma lista de números e 
+exiba apenas os pares usando a função filter().
+'''
+
+
+lista = input('Digite os números separados por espaço:  ').split()
+
+pares = filter(lambda x: int(x) % 2 == 0, lista)
+print('Numeros pares:', " ".join(pares))
