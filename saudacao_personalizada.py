7f9fe2d4254ba3751778aebd6025bb010c48100d
Raphael Silvestre Alves <ssevlaa@gmail.com>
2026-04-16 13:30:54 -0300

    add: saudacao-personalizada

---
+++ saudacao_personalizada.py
@@ -0,0 +1,23 @@
+'''
+Beatriz está desenvolvendo um sistema de atendimento para um site de serviços. 
+Ela deseja criar um programa que exiba uma saudação personalizada dependendo
+ da hora do dia que o usuário acessa a plataforma. 
+ O sistema deverá ter a seguinte regra:
+
+Se for antes das 12h, exibir "Bom dia";
+
+Entre 12h e 18h, exibir "Boa tarde";
+
+Após 18h, exibir "Boa noite".
+'''
+
+def saudação(hora_atual):
+    if hora_atual < 12:
+        print('Bom dia')
+    elif hora_atual < 18:
+        print('Boa tarde')
+    else:
+        print('Boa noite')
+
+hora_atual = int(input('Digite a hora atual (0-23): '))
+saudação(hora_atual)
